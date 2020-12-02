abc6b5f95ce1ece4c1e5d20f3386219b3446a0e4
jose mendoza <chea.mendez396@gmail.com>
2020-12-01 21:50:35 -0400

    agregacion del componente swiper

---
+++ src/componentes/index.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Text, FlatList, Image, StyleSheet, Dimensions, View, ScrollView, TextInput} from 'react-native';
+
+var {height, width } = Dimensions.get('window');
+import Swiper from 'react-native-swiper'
+
+export default class App extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      dataBanner:[]
+    }
+  }
+  
+  componentDidMount()
+  {
+    const url = "http://tutofox.com/foodapp/api.json";
+    return fetch(url)
+    .then((response) => response.json())
+    .then((responseJson)=>{
+      this.setState({
+        isLoading: false,
+        dataBanner: responseJson.banner
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <View style={{ flex: 1, backgroundColor:"#f2f2f2" }}>
+          <View style={{width: width, alignItems:'center'}} >
+              <Image resizeMode="contain" style={{height:60, width:width/2, margin:10 }} source={{url:'https://tutofox.com/foodapp/foodapp.png'}} />
+              <Swiper style={{height:width/2}}  showsButtons={false} autoplay={true} autoplayTimeout={3}>
+                {
+                  this.state.dataBanner.map((itemmap)=>{
+                    return(
+                      <Image style={styles.imageBanner} resizeMode="contain" source={{uri:itemmap}}/>
+                    )
+                  })
+                }
+              </Swiper>
+              <View style={{height:20}} />
+          </View>
+          <Text>App Barney Gomez</Text>
+          <Text>{JSON.stringify(this.state.dataBanner)}</Text>
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  imageBanner: {
+    height:width/2,
+    width:width-40,
+    borderRadius:10,
+    marginHorizontal:20
+  },
+});
