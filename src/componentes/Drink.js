06a9e1bc10960c317bddafdfe65f9741f7149d25
jose mendoza <chea.mendez396@gmail.com>
2020-12-09 19:24:21 -0400

    agregacion y navegacion de tabs

---
+++ src/componentes/Drink.js
@@ -0,0 +1,152 @@
+import React, { Component } from 'react';
+import { Text, FlatList, TouchableOpacity, Image, StyleSheet, Dimensions, View, ScrollView, TextInput} from 'react-native';
+
+
+
+var {height, width } = Dimensions.get('window');
+import Swiper from 'react-native-swiper'
+
+export default class App extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      dataBanner:[],
+      dataCategories:[],
+      dataDrink:[],
+      selectCatg:0
+    }
+  }
+
+  
+  componentDidMount()
+  {
+    // const url = "http://tutofox.com/foodapp/api.json";
+    const url = "https://cristiansandyflores.github.io/paginaExamen/data.json";
+    return fetch(url)
+    .then((response) => response.json())
+    .then((responseJson)=>{
+      this.setState({
+        isLoading: false,
+        dataBanner: responseJson.banner,
+        dataCategories: responseJson.categories,
+        dataDrink: responseJson.drinks
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <View style={{ flex: 1, backgroundColor:"#f2f2f2" }}>
+          <View style={{width: width, alignItems:'center'}} >
+              <Image resizeMode="contain" style={{height:60, width:width/2, margin:10 }} source={{url:'https://detarijalomejor.com/avisos/wp-content/uploads/2020/08/logo.jpg'}} />
+              <Swiper style={{height:width/2}}  showsButtons={false} autoplay={true} autoplayTimeout={3}>
+                {
+                  this.state.dataBanner.map((itemmap)=>{
+                    return(
+                      <Image style={styles.imageBanner} resizeMode="contain" source={{uri:itemmap}}/>
+                    )
+                  })
+                }
+              </Swiper>
+          </View>
+          <View style={{width:width, borderRadius:20, paddingVertical:20, backgroundColor: 'white'}}>
+              <Text style={styles.titleCatg}>Categorías {this.state.selectCatg}</Text>
+          <FlatList
+            horizontal={true}
+            data={this.state.dataCategories}
+            renderItem={({item})=>this._renderItem(item)}
+            keyExtractor = {(item, index)=>index.toString()}
+          />
+          <FlatList
+            data={this.state.dataDrink}
+            numColumns={2}
+            renderItem={({item})=>this._renderItemDrink(item)}
+            keyExtractor = {(item, index)=>index.toString()}
+          />
+          </View> 
+        </View>
+      </ScrollView>
+    );
+  }
+
+  _renderItemDrink(item){
+    let catg = this.state.selectCatg
+    if (catg==0||item.categorie) {
+      return(
+        <TouchableOpacity style={styles.divDrink}>
+          <Image style={styles.imageDrink}
+          resizeMode="contain" source={{uri:item.image}}/>
+          <View style={{height:((width/2)-20)-90, width:((width/2)-20)-10, backgroundColor:'transparent'}}/>
+          <Text style={{fontWeight: 'bold', fontSize:22, textAlign:'center'}}>{item.name}</Text>
+          <Text>Descp Drink and Details </Text>
+      <Text style={{fontSize:20, color:'green'}}>Bs{item.price}</Text>
+        </TouchableOpacity>
+      )
+    }
+  }
+
+  _renderItem(item){
+    return(
+      <TouchableOpacity 
+      onPress={()=>this.setState({selectCatg:item.id})}
+      style={[styles.divCategories,{backgroundColor:item.color}]}>
+        <Image
+          style={{width:100, height:80}}
+          resizeMode="contain"
+          source={{uri:item.image}} />
+          <Text style={{fontWeight:'bold', fontSize:22}}>
+            {item.name}
+          </Text>
+          </TouchableOpacity>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  imageBanner: {
+    height:width/2,
+    width:width-40,
+    borderRadius:10,
+    marginHorizontal:20
+  },
+  titleCatg:{
+    fontSize:30,
+    fontWeight:'bold',
+    textAlign:'center',
+    marginBottom:10
+  },
+  divCategories:{
+    backgroundColor:'red',
+    margin:5, 
+    alignItems:'center',
+    borderRadius:10,
+    padding:10
+  },
+  imageDrink:{
+    width:((width/2)-20)-10,
+    height:((width/2)-20)-30,
+    backgroundColor: 'transparent',
+    position:'absolute',
+    top:-45
+  },
+  divDrink:{
+    width:(width/2)-20,
+    backgroundColor: 'red',
+    padding:10,
+    borderRadius:10,
+    marginTop:55,
+    marginBottom:5,
+    marginLeft:10,
+    alignItems:'center',
+    borderWidth:1,
+    elevation:8,
+    shadowOpacity:0.3,
+    shadowRadius:50,
+    backgroundColor:'white'
+  }
+});
--- src/componentes/index.js
+++ src/componentes/index.js
@@ -1,152 +1,94 @@
 import React, { Component } from 'react';
-import { Text, FlatList, TouchableOpacity, Image, StyleSheet, Dimensions, View, ScrollView, TextInput} from 'react-native';
+import { Text, View, TextInput, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
+import Drink from "./Drink";
+import Cart from "./Cart";
+import Address from "./Address";
+import Profile from "./Profile";
 
+var { width } = Dimensions.get("window");
+import Icon from 'react-native-vector-icons/Ionicons';
 
+console.disableYellowBox = true;
 
-var {height, width } = Dimensions.get('window');
-import Swiper from 'react-native-swiper'
+export default class Index extends Component {
 
-export default class App extends Component {
-  constructor(props)
-  {
-    super(props);
-    this.state = {
-      dataBanner:[],
-      dataCategories:[],
-      dataDrink:[],
-      selectCatg:0
-    }
-  }
-
-  
-  componentDidMount()
-  {
-    //const url = "http://tutofox.com/foodapp/api.json";
-    const url = "https://cristiansandyflores.github.io/paginaExamen/data.json";
-    return fetch(url)
-    .then((response) => response.json())
-    .then((responseJson)=>{
-      this.setState({
-        isLoading: false,
-        dataBanner: responseJson.banner,
-        dataCategories: responseJson.categories,
-        dataDrink: responseJson.drinks
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  constructor(props) {
+     super(props);
+     this.state = {
+       module:1,
+     };
   }
 
   render() {
     return (
-      <ScrollView>
-        <View style={{ flex: 1, backgroundColor:"#f2f2f2" }}>
-          <View style={{width: width, alignItems:'center'}} >
-              <Image resizeMode="contain" style={{height:60, width:width/2, margin:10 }} source={{url:'https://detarijalomejor.com/avisos/wp-content/uploads/2020/08/logo.jpg'}} />
-              <Swiper style={{height:width/2}}  showsButtons={false} autoplay={true} autoplayTimeout={3}>
-                {
-                  this.state.dataBanner.map((itemmap)=>{
-                    return(
-                      <Image style={styles.imageBanner} resizeMode="contain" source={{uri:itemmap}}/>
-                    )
-                  })
-                }
-              </Swiper>
-          </View>
-          <View style={{width:width, borderRadius:20, paddingVertical:20, backgroundColor: 'white'}}>
-              <Text style={styles.titleCatg}>Categorías {this.state.selectCatg}</Text>
-          <FlatList
-            horizontal={true}
-            data={this.state.dataCategories}
-            renderItem={({item})=>this._renderItem(item)}
-            keyExtractor = {(item, index)=>index.toString()}
-          />
-          <FlatList
-            data={this.state.dataDrink}
-            numColumns={2}
-            renderItem={({item})=>this._renderItemDrink(item)}
-            keyExtractor = {(item, index)=>index.toString()}
-          />
-          </View> 
-        </View>
-      </ScrollView>
-    );
-  }
-
-  _renderItemDrink(item){
-    let catg = this.state.selectCatg
-    if (catg==0||item.categorie) {
-      return(
-        <TouchableOpacity style={styles.divDrink}>
-          <Image style={styles.imageDrink}
-          resizeMode="contain" source={{uri:item.image}}/>
-          <View style={{height:((width/2)-20)-90, width:((width/2)-20)-10, backgroundColor:'transparent'}}/>
-          <Text style={{fontWeight: 'bold', fontSize:22, textAlign:'center'}}>{item.name}</Text>
-          <Text>Descp Drink and Details </Text>
-      <Text style={{fontSize:20, color:'green'}}>Bs{item.price}</Text>
-        </TouchableOpacity>
-      )
-    }
-  }
-
-  _renderItem(item){
-    return(
-      <TouchableOpacity 
-      onPress={()=>this.setState({selectCatg:item.id})}
-      style={[styles.divCategories,{backgroundColor:item.color}]}>
-        <Image
-          style={{width:100, height:80}}
-          resizeMode="contain"
-          source={{uri:item.image}} />
-          <Text style={{fontWeight:'bold', fontSize:22}}>
-            {item.name}
-          </Text>
+      <View style={{flex:1}}>
+        {
+          this.state.module==1?
+          <Drink/>
+          :this.state.module==2?
+          <Cart/>
+          :this.state.module==3?
+          <Address/>
+          :<Profile/>
+        }
+        <View style={styles.bottonTab}>
+          <TouchableOpacity onPress={()=>this.setState({module:1})}>
+            <View style={styles.itemTab}>
+              <Icon name="beer-outline" size={30} color={this.state.module==1?"#900":"gray"} />
+              <Text>
+                Tragos
+              </Text>
+            </View>
           </TouchableOpacity>
-    )
+          
+          <TouchableOpacity onPress={()=>this.setState({module:2})}>
+            <View style={styles.itemTab}>
+              <Icon name="basket" size={30} color={this.state.module==2?"#900":"gray"} />
+              <Text>
+                Carrito
+              </Text>
+            </View>
+          </TouchableOpacity>
+          
+          <TouchableOpacity onPress={()=>this.setState({module:3})}>
+            <View style={styles.itemTab}>
+              <Icon name="map" size={30} color={this.state.module==3?"#900":"gray"} />
+              <Text>
+                Direccion
+              </Text>
+            </View>
+          </TouchableOpacity>
+          
+          <TouchableOpacity onPress={()=>this.setState({module:4})}>
+            <View style={styles.itemTab}>
+              <Icon name="person-circle" size={30} color={this.state.module==4?"#900":"gray"} />
+              <Text>
+                Usuario
+              </Text>
+            </View>
+          </TouchableOpacity>
+
+        </View>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  imageBanner: {
-    height:width/2,
-    width:width-40,
-    borderRadius:10,
-    marginHorizontal:20
-  },
-  titleCatg:{
-    fontSize:30,
-    fontWeight:'bold',
-    textAlign:'center',
-    marginBottom:10
-  },
-  divCategories:{
-    backgroundColor:'red',
-    margin:5, 
-    alignItems:'center',
-    borderRadius:10,
-    padding:10
-  },
-  imageDrink:{
-    width:((width/2)-20)-10,
-    height:((width/2)-20)-30,
-    backgroundColor: 'transparent',
-    position:'absolute',
-    top:-45
-  },
-  divDrink:{
-    width:(width/2)-20,
-    backgroundColor: 'red',
-    padding:10,
-    borderRadius:10,
-    marginTop:55,
-    marginBottom:5,
-    marginLeft:10,
-    alignItems:'center',
-    borderWidth:1,
+  bottonTab:{
+    width:width,
+    backgroundColor:"gray",
+    height:60,
+    flexDirection:"row",
     elevation:8,
     shadowOpacity:0.3,
     shadowRadius:50,
-    backgroundColor:'white'
+  },
+  itemTab:{
+    width:width/4,
+    backgroundColor:"white",
+    alignItems:'center',
+    justifyContent:'center',
+    height:60,
   }
-});
+})
